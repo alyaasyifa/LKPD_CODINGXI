288b96ce3fc14e3281058a38dc38ec573c635385
alyaasyifa <128487214+alyaasyifa@users.noreply.github.com>
2023-08-23 10:21:43 +0700

    Add files via upload

---
+++ NO16.php
@@ -0,0 +1,13 @@
+<?php
+for ($i = 1; $i <= 50; $i++) {
+    if ($i % 2 == 1) {
+        echo $i . ". Ganjil";
+        echo "</br>";
+        echo "</br>";
+    }else{
+        echo $i. ". Genap";
+        echo "</br>";
+        echo "</br>";
+    }
+}
+?>
